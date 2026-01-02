60bfe489b1b57b992761980ef83a55bcdf4c11a5
Yassine Jedidi <yassinejedidi17@gmail.com>
2026-01-02 21:01:47 +0100

    add sparkles banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { SparkleBanner } from "@/components/home/sparkle-banner";
 
 export default function Home() {
   return (
-    <>
-      {/* Root Layout */}
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">{/* Page content goes here */}</main>
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <SparkleBanner />
+      </main>
+      <Footer />
+    </div>
   );
 }
